464bcc9f490e3f89879ca5c205825f4e01abce6f
Suman <pasisuman2511@gmail.com>
2025-09-20 18:18:01 +0530

    Second Commit

--- Pattern_solving/src/pattern/P3.java
+++ Pattern_solving/src/pattern/P3.java
@@ -2,16 +2,26 @@ package pattern;
 
 public class P3 {
 	public static void main(String[] args) {
-		pattern3(5);
+//		pattern3(5);
+		patt(4);
 	}
-	static void pattern3(int n) {
+//	static void pattern3(int n) {
+//		for(int row = 1; row<=n; row++) {
+//			for(int col=n; col>=row; col--) {
+//				System.out.print("* ");
+//			}
+//			System.out.println();
+//		}
+	
+	static void patt(int n) {
 		for(int row = 1; row<=n; row++) {
-			for(int col=n; col>=row; col--) {
+			for(int col=1; col <= n-row+1; col++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
 	}
+	
 }
 
 //* * * * * 
